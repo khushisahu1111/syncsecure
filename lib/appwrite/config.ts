15ed1805909cf4fb1d6602245b0f1c3f20775470
milanawasthi18 <milanawasthi726@gmail.com>
2026-03-27 15:32:15 +0530

    first commit

--- lib/appwrite/config.ts
+++ lib/appwrite/config.ts
@@ -1,4 +1,4 @@
-export const appwriteConfig = {
+ export const appwriteConfig = {
   endpointUrl: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT!,
   projectId: process.env.NEXT_PUBLIC_APPWRITE_PROJECT!,
   databaseId: process.env.NEXT_PUBLIC_APPWRITE_DATABASE!,
